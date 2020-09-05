80b8ec1e82cbe74d6892f61bbb8626399a0c3911
Jourdan Rentschler <jrentschler3@gmail.com>
2020-09-04 19:31:51 -0600

    prompted user menu

--- src/com/skilldistillery/foodproject/FoodTruck.java
+++ src/com/skilldistillery/foodproject/FoodTruck.java
@@ -10,5 +10,37 @@ public class FoodTruck {
 	public FoodTruck() {
 		
 	}
+	public FoodTruck(String name, String type, double rating) {
+		this.name = name;
+		this.type = type;
+		this.rating = rating;
+		
+//		getters/setters
+	}
+	public int getNextUniqueNumTruckId() {
+		return nextUniqueNumTruckId;
+	}
+	public void setNextUniqueNumTruckId() {
+		this.nextUniqueNumTruckId = FoodTruck.uniqueNumTruckId++;
+		
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public double getRating() {
+		return rating;
+	}
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
 
 }
--- src/com/skilldistillery/foodproject/FoodTruckApp.java
+++ src/com/skilldistillery/foodproject/FoodTruckApp.java
@@ -14,10 +14,46 @@ public class FoodTruckApp {
 	}
 	private void generate() {
 		int count = 0;
-		
+//		user story #1
 		Scanner kb = new Scanner(System.in);
 		String inputName = "";
 		String inputType = "";
 		double inputRating;
+		
+		System.out.println("-------------------------");
+//		create food menu here 
+		
+		
+		
+//		user story #2
+		while (count < 5) {
+			brandNewTruck = new FoodTruck();
+			System.out.println("Please Enter Truck's Name: ");
+			inputName = kb.nextLine();
+			
+			if(inputName.equalsIgnoreCase("Quit")) {
+				break;
+			}
+			System.out.println("Please Enter Food You Like: ");
+			inputType = kb.nextLine();
+			
+			if(inputType.equalsIgnoreCase("Quit")) {
+				break;
+			}
+			System.out.println("Please Enter Truck's Rating: ");
+			inputRating = kb.nextDouble();
+//			kb.nextLine();
+			
+			brandNewTruck.setName (inputName);
+			brandNewTruck.setType (inputType);
+			brandNewTruck.setRating (inputRating);
+			brand.NewTruck.setnextUniqueNumTruckId ();
+			additionalBrandNewTruck(brandNewTruck);
+			count ++;
+			
+		}
+		
+		
+		
 	}
 }
