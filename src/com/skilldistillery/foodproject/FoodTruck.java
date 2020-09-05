52458efc41c6be97dffe0dc1d84e5b068ad1a01b
Jourdan Rentschler <jrentschler3@gmail.com>
2020-09-05 10:14:31 -0600

    FoodTruck and App are drafted

--- src/com/skilldistillery/foodproject/FoodTruck.java
+++ src/com/skilldistillery/foodproject/FoodTruck.java
@@ -1,46 +1,74 @@
 package com.skilldistillery.foodproject;
 
 public class FoodTruck {
+//	INITIALIZING THE CLASS DATATYPES 
 	private static int uniqueNumTruckId = 12345;
 	private int nextUniqueNumTruckId;
 	private String name;
 	private String type;
 	private double rating;
 	
+//	default ctor(constructor)
 	public FoodTruck() {
-		
+
 	}
+//	Methods ctor (constructors)
 	public FoodTruck(String name, String type, double rating) {
 		this.name = name;
 		this.type = type;
 		this.rating = rating;
-		
-//		getters/setters
 	}
+
+//		getter: 
 	public int getNextUniqueNumTruckId() {
 		return nextUniqueNumTruckId;
 	}
+//		setter:
 	public void setNextUniqueNumTruckId() {
 		this.nextUniqueNumTruckId = FoodTruck.uniqueNumTruckId++;
-		
 	}
+
+//		getter: 
 	public String getName() {
 		return name;
 	}
+
+//		setter:
 	public void setName(String name) {
 		this.name = name;
 	}
+
+//		getter: 
 	public String getType() {
 		return type;
 	}
+
+//		setter:
 	public void setType(String type) {
 		this.type = type;
 	}
+
+//		getter: 
 	public double getRating() {
 		return rating;
 	}
+
+//		setter:
 	public void setRating(double rating) {
 		this.rating = rating;
 	}
-
+	
+	
+	@Override
+	public String toString() {
+		
+		return "FoodTruck [nextUniqueNumTruckId=" + nextUniqueNumTruckId + ", name=" + name + ", type=" + type
+				+ ", rating=" + rating + "]";
+	}
+	public void displayFoodTruck() {
+		String FoodTruck = this.toString();
+		System.out.println(FoodTruck);
+		
+	}
+	
 }
--- src/com/skilldistillery/foodproject/FoodTruckApp.java
+++ src/com/skilldistillery/foodproject/FoodTruckApp.java
@@ -3,15 +3,22 @@ package com.skilldistillery.foodproject;
 import java.util.Scanner;
 
 public class FoodTruckApp {
-	private FoodTruck[] foodTruck = new FoodTruck[5];
-	private FoodTruck brandNewTruck;
+	private FoodTruck[] foodTruckArr = new FoodTruck[5];
+	private FoodTruck bNewTruck;
 	private int index =0;
-
+	
+//	MAIN METHOD:
 	public static void main(String[] args) {
 		FoodTruckApp userInput = new FoodTruckApp();
 		
+		
+		
+		
+		
+		
 		userInput.generate();
 	}
+//	METHOD 1: GENERATE USER OUTPUT
 	private void generate() {
 		int count = 0;
 //		user story #1
@@ -20,14 +27,24 @@ public class FoodTruckApp {
 		String inputType = "";
 		double inputRating;
 		
-		System.out.println("-------------------------");
-//		create food menu here 
+//		create food menu here FOR THE USER TO INPUT
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("WELCOME, TO THE FOOD TRUCK APP :");
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("{} GO AHEAD AND RATE THE FIVE FOOD TRUCKS.");
+		System.out.println("{} PLEASE PROVIDE THE NAME, FOOD TYPE, AND RATING (0-10 SCALE) FOR THE FIVE FOOD TRUCKS.");
+		System.out.println("{} THE SCALE FOR THE RATING WILL BE DEFINED AS FOLLOWS: ");
+		System.out.println("{} SCALE: 0  ---> THE LEAST FAVORITE THING YOU HAVE EVER ATE!");
+		System.out.println("-------------------------TO---------------------------------");
+		System.out.println("{} SCALE: 10 ---> THE BEST THING YOU HAVE EVER HAD!!!");
+		
 		
 		
 		
 //		user story #2
 		while (count < 5) {
-			brandNewTruck = new FoodTruck();
+			bNewTruck = new FoodTruck();
 			System.out.println("Please Enter Truck's Name: ");
 			inputName = kb.nextLine();
 			
@@ -44,16 +61,75 @@ public class FoodTruckApp {
 			inputRating = kb.nextDouble();
 //			kb.nextLine();
 			
-			brandNewTruck.setName (inputName);
-			brandNewTruck.setType (inputType);
-			brandNewTruck.setRating (inputRating);
-			brand.NewTruck.setnextUniqueNumTruckId ();
-			additionalBrandNewTruck(brandNewTruck);
+			bNewTruck.setName (inputName);
+			bNewTruck.setType (inputType);
+			bNewTruck.setRating (inputRating);
+			bNewTruck.setNextUniqueNumTruckId ();
+			additionalBNewTruck(bNewTruck);
 			count ++;
 			
+		
+		int select = 0;
+		
+		do {
+			user story #3
+			have the user input their enteries here 
+			System.out.println("------------------------------------");
+			System.out.println("------------------------------------");
+			System.out.println("------------------------------------");
+			
+			
+			
+			select = kb.nextInt();
+			
+//		sysout lines closing the menu selection
+		
+		
 		}
-		
-		
+		private void additionalBNewTruck(FoodTruck brandNewTruck2) {
+		foodTruckArr[index] = brandNewTruck2;
+		index++;
 		
 	}
-}
+		private FoodTruck[] getNewTrucks () {
+			FoodTruck[] copyTrucks = new FoodTruck[index];
+			
+			for (int i = 0; i < copyTrucks.length; i++) {
+				copyTrucks[i] = foodTruckArr[i];
+				
+			}
+			return copyTrucks;
+		}
+			private void getAverage(FoodTruck[] brandNewTruck) {
+				double average = 0;
+				double sum =0 ;
+				
+			}
+			private void printfoodTruck(FoodTruck[] brandNewtTruck) {
+				for (FoodTruck truck : bNewTruck) {
+					bNewTruck.displayBNewTruck();
+				}
+				
+			private void getHighest(FoodTruck[] brandNewTruck) {
+				double highest = 0;
+				int uniqueTruckId = 0;
+				String truckName = "";
+				String truckType = "";
+				for (int i = 0; i < brandNewTruck.length; i++) {
+					double ratings = bNewTruck.getRating();
+				}
+				if (ratings > highest) {
+					ratings = highest;
+					
+				}
+				System.out.println("--------------------------------------------------------------");
+				System.out.println("");
+				System.out.println("");
+				System.out.println("");
+				System.out.println("");
+				System.out.println("");
+					
+				
+				}
+	}
+
